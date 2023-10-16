2228a8bdaf805c56a79ae7cd87a764aa0e54bc59
Sandy M <sandy5501m@gmail.com>
2023-10-16 22:01:19 +0530

    create form layout design

--- RCS-communication/Forms/FormCreateInstruction.Designer.cs
+++ RCS-communication/Forms/FormCreateInstruction.Designer.cs
@@ -29,53 +29,51 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbtnInbound = new System.Windows.Forms.RadioButton();
+            this.rbtnOutbound = new System.Windows.Forms.RadioButton();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.groupBoxInbound = new System.Windows.Forms.GroupBox();
+            this.groupBoxOutbound = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBoxInbound.SuspendLayout();
+            this.groupBoxOutbound.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(702, 486);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(625, 351);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 61);
+            this.button1.Size = new System.Drawing.Size(147, 49);
             this.button1.TabIndex = 2;
             this.button1.Text = "GO";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(172, 160);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(474, 26);
-            this.textBox3.TabIndex = 5;
-            // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(172, 220);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Location = new System.Drawing.Point(146, 389);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(474, 26);
+            this.textBox4.Size = new System.Drawing.Size(422, 22);
             this.textBox4.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(31, 160);
+            this.label3.Location = new System.Drawing.Point(21, 341);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(57, 24);
+            this.label3.Size = new System.Drawing.Size(49, 21);
             this.label3.TabIndex = 7;
             this.label3.Text = "Type";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -84,9 +82,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(19, 220);
+            this.label4.Location = new System.Drawing.Point(10, 389);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 24);
+            this.label4.Size = new System.Drawing.Size(78, 21);
             this.label4.TabIndex = 8;
             this.label4.Text = "Priority";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -95,9 +93,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(24, 275);
+            this.label5.Location = new System.Drawing.Point(23, 41);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(86, 25);
+            this.label5.Size = new System.Drawing.Size(68, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "Source";
             this.label5.Click += new System.EventHandler(this.label5_Click);
@@ -106,39 +104,37 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 330);
+            this.label6.Location = new System.Drawing.Point(13, 85);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(131, 25);
+            this.label6.Size = new System.Drawing.Size(105, 20);
             this.label6.TabIndex = 12;
             this.label6.Text = "Destination";
             // 
-            // radioButton1
+            // rbtnInbound
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(290, 83);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(117, 28);
-            this.radioButton1.TabIndex = 13;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Inbound";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.rbtnInbound.AutoSize = true;
+            this.rbtnInbound.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnInbound.Location = new System.Drawing.Point(40, 83);
+            this.rbtnInbound.Name = "rbtnInbound";
+            this.rbtnInbound.Size = new System.Drawing.Size(100, 25);
+            this.rbtnInbound.TabIndex = 13;
+            this.rbtnInbound.TabStop = true;
+            this.rbtnInbound.Text = "Inbound";
+            this.rbtnInbound.UseVisualStyleBackColor = true;
+            this.rbtnInbound.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // radioButton2
+            // rbtnOutbound
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(482, 83);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(129, 28);
-            this.radioButton2.TabIndex = 14;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Outbound";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.rbtnOutbound.AutoSize = true;
+            this.rbtnOutbound.Font = new System.Drawing.Font("Modern No. 20", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnOutbound.Location = new System.Drawing.Point(403, 83);
+            this.rbtnOutbound.Name = "rbtnOutbound";
+            this.rbtnOutbound.Size = new System.Drawing.Size(110, 25);
+            this.rbtnOutbound.TabIndex = 14;
+            this.rbtnOutbound.TabStop = true;
+            this.rbtnOutbound.Text = "Outbound";
+            this.rbtnOutbound.UseVisualStyleBackColor = true;
+            this.rbtnOutbound.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // comboBox1
             // 
@@ -146,56 +142,123 @@
             this.comboBox1.Items.AddRange(new object[] {
             "Source 1",
             "Source 2"});
-            this.comboBox1.Location = new System.Drawing.Point(172, 276);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Location = new System.Drawing.Point(155, 42);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(136, 28);
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
             this.comboBox1.TabIndex = 15;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "Destination 1",
-            "Destination 2"});
-            this.comboBox2.Location = new System.Drawing.Point(172, 327);
-            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(136, 28);
-            this.comboBox2.TabIndex = 16;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Stencil", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(251, 11);
+            this.label7.Location = new System.Drawing.Point(223, 9);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(399, 43);
+            this.label7.Size = new System.Drawing.Size(338, 35);
             this.label7.TabIndex = 17;
             this.label7.Text = "Create Instruction ";
             this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
+            // groupBoxInbound
+            // 
+            this.groupBoxInbound.Controls.Add(this.textBox1);
+            this.groupBoxInbound.Controls.Add(this.label5);
+            this.groupBoxInbound.Controls.Add(this.label6);
+            this.groupBoxInbound.Controls.Add(this.comboBox1);
+            this.groupBoxInbound.Location = new System.Drawing.Point(40, 132);
+            this.groupBoxInbound.Name = "groupBoxInbound";
+            this.groupBoxInbound.Size = new System.Drawing.Size(294, 169);
+            this.groupBoxInbound.TabIndex = 18;
+            this.groupBoxInbound.TabStop = false;
+            this.groupBoxInbound.Text = "groupBoxInbound";
+            // 
+            // groupBoxOutbound
+            // 
+            this.groupBoxOutbound.Controls.Add(this.textBox2);
+            this.groupBoxOutbound.Controls.Add(this.label1);
+            this.groupBoxOutbound.Controls.Add(this.label2);
+            this.groupBoxOutbound.Controls.Add(this.comboBox3);
+            this.groupBoxOutbound.Location = new System.Drawing.Point(403, 132);
+            this.groupBoxOutbound.Name = "groupBoxOutbound";
+            this.groupBoxOutbound.Size = new System.Drawing.Size(294, 169);
+            this.groupBoxOutbound.TabIndex = 19;
+            this.groupBoxOutbound.TabStop = false;
+            this.groupBoxOutbound.Text = "groupBoxOutbound";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Destination";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Destination 1",
+            "Destination 2"});
+            this.comboBox3.Location = new System.Drawing.Point(155, 83);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(146, 342);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(422, 24);
+            this.comboBox2.TabIndex = 20;
+            // 
             // FormCreateInstruction
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(900, 562);
-            this.Controls.Add(this.label7);
+            this.ClientSize = new System.Drawing.Size(802, 457);
             this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBoxOutbound);
+            this.Controls.Add(this.groupBoxInbound);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.rbtnOutbound);
+            this.Controls.Add(this.rbtnInbound);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormCreateInstruction";
             this.Text = "FormCreateInstruction";
+            this.groupBoxInbound.ResumeLayout(false);
+            this.groupBoxInbound.PerformLayout();
+            this.groupBoxOutbound.ResumeLayout(false);
+            this.groupBoxOutbound.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,16 +266,22 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbtnInbound;
+        private System.Windows.Forms.RadioButton rbtnOutbound;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBoxInbound;
+        private System.Windows.Forms.GroupBox groupBoxOutbound;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
