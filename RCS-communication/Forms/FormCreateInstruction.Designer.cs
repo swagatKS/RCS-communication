20f9659fc9def0cd5068b7fc929623bb043af94c
Sandy M <sandy5501m@gmail.com>
2023-10-16 12:48:52 +0530

    Revert "testing"
    This reverts commit 4f4e5e7e0eea714a553f9d8a3461b268a442830b.

--- RCS-communication/Forms/FormCreateInstruction.Designer.cs
+++ RCS-communication/Forms/FormCreateInstruction.Designer.cs
@@ -28,17 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // FormCreateInstruction
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "FormCreateInstruction";
             this.Text = "FormCreateInstruction";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
