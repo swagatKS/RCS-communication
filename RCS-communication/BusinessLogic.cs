12a893bbe1dfa66b511a5f01065c355f88f937db
Swagat <ksswagat78@gmail.com>
2023-10-30 19:24:41 +0530

    Adding Bussiness Logic class

---
+++ RCS-communication/BusinessLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCS_communication
+{
+    public static class BusinessLogic
+    {
+        public static string GenerateId()
+        {
+            string formattedDate = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string dateTimeMilli = formattedDate + DateTime.Now.Millisecond.ToString();
+            string id = "T" + dateTimeMilli;
+            return id;
+        }
+    }
+}
